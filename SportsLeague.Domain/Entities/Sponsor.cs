77c2a2bc888ba7e0d78436b3867fed483f0034bd
Sebastian López <sebasgal608@gmail.com>
2026-04-14 23:00:53 -0500

    I create Entities and enum, update LeagueDbContext

---
+++ SportsLeague.Domain/Entities/Sponsor.cs
@@ -0,0 +1,20 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Entities;
+
+public class Sponsor : AuditBase
+{
+    public string? Name { get; set; }
+
+    public string? ContactEmail { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? WebsiteUrl { get; set; }
+
+    public SponsorCategory Category { get; set; }
+
+    // Navigation Property (Relación N:M)
+    public ICollection<TournamentSponsor> TournamentSponsors { get; set; }
+        = new List<TournamentSponsor>();
+}
---
+++ SportsLeague.Domain/Entities/TournamentSponsor.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsLeague.Domain.Entities;
+
+public class TournamentSponsor : AuditBase
+{
+    public int TournamentId { get; set; }
+    public Tournament Tournament { get; set; } = null!;
+
+    public int SponsorId { get; set; }
+    public Sponsor Sponsor { get; set; } = null!;
+}
