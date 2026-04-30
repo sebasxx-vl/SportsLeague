33e84e313e584cb8b4f41cefe64e7ed5df36bec9
Sebastian López <sebasgal608@gmail.com>
2026-04-29 20:03:56 -0500

    Nothing

--- SportsLeague.Domain/Entities/Referee.cs
+++ SportsLeague.Domain/Entities/Referee.cs
@@ -6,7 +6,5 @@
         public string LastName { get; set; } = string.Empty;
         public string Nationality { get; set; } = string.Empty;
 
-        // Navigation Property - Colección de partidos arbitrados por el árbitro
-        public ICollection<Match> Matches { get; set; } = new List<Match>();
     }
 }
